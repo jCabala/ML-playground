e4a64169f5a163a9deae8a82c0265ee650202191
Jakub Cabala <kuba.cabala6@gmail.com>
2023-12-01 10:12:02 +0000

    Cleand some functions in the regression model and evaluated the metrics

--- machine-learning-with-python-for-beginners-projects/regression/model.py
+++ machine-learning-with-python-for-beginners-projects/regression/model.py
@@ -5,10 +5,39 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('Agg')
+
+def transform(data):
+    # Data preprocessing
+    num_col = data.columns[data.columns != "Category"]
+    cat_col = ["Category"]
+
+    num_preprocessing = Pipeline([
+        ('imp', SimpleImputer(strategy="mean")),
+        ("scaler", StandardScaler())
+    ])
+
+    full_preprocessing = ColumnTransformer([
+        ('num', num_preprocessing, num_col),
+        ('cat', OneHotEncoder(
+            dtype=int, sparse_output=False, drop='first'), cat_col)
+    ])
+
+    return full_preprocessing.fit_transform(data)
+
+def predict_and_print_metrics(lr, X, y):
+    y_predicted = lr.predict(X)
+
+    r2 = r2_score(y, y_predicted)
+    RMSE = mean_squared_error(y, y_predicted, squared=False)
+    print(f"RMSE={RMSE} r2={r2}")
+    return y_predicted
 
 # Plotting histograms for each column
 df = pd.read_csv('reviews.csv')
@@ -25,7 +54,7 @@ for col in df:
 # Saving the data description
 print(df.describe(), file=open("data_description.txt", "w"))
 
-# The description and histogram say that "prica" and "DSTLU" 
+# The description and histogram say that "price" and "DSTLU" 
 # columns have negarive values. That is likely an error.
 numInvalid = (len(df[df["Days since Last Update"] < 0]) + 
               len(df[df["Price"] < 0]))
@@ -51,24 +80,19 @@ for col in X_train:
     plt.savefig(f"scatter_plots/{col}-plot.png")
     plt.clf()
 
-# Data preprocessing
-num_col = X.columns[X.columns != "Category"]
-cat_col = ["Category"]
-
-imp = SimpleImputer(strategy="mean")
-tf_num = imp.fit_transform(X_train[num_col])
-
-scaler = StandardScaler()
-tf_num = scaler.fit_transform(tf_num)
-
-ohe = OneHotEncoder(dtype=int, sparse_output=False, drop='first')
-tf_cat = ohe.fit_transform(X_train[cat_col])
-
-X_train_transformed = np.concatenate((tf_num, tf_cat), axis=1)
-
 # Training the model
+X_train_transformed = transform(X_train) 
 model =  LinearRegression()
 model.fit(X_train_transformed, y_train)
 
 print("Coefficients of the model (a0, [a1, ..., an]):")
 print(model.intercept_, model.coef_)
+
+# Evaluating on the training set
+print("Metrics on the training set")
+y_train_pred = predict_and_print_metrics(model, X_train_transformed, y_train)
+
+# Evaluating on the test set
+print("Metrics on the test set")
+X_test_transformed = transform(X_test)
+predict_and_print_metrics(model, X_test_transformed, y_test)
