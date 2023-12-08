ee79299281a54cc0d4efa3677ec44fb2637b52e7
Jakub Cabala <kuba.cabala6@gmail.com>
2023-12-08 19:00:37 +0000

    Divided the set into X and y. Plotted some digits

--- machine-learning-with-python-for-beginners-projects/classification/model.py
+++ machine-learning-with-python-for-beginners-projects/classification/model.py
@@ -1,8 +1,18 @@
 import numpy as np
 import pandas as pd
+
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+
 from sklearn.datasets import load_digits
 
 digits = load_digits()
-print(type(digits))
-print(type(digits.data))
-print(digits.data.shape)
+X = digits.data
+y = digits.target
+
+# Displaying the first instance as an image
+some_digit = X[8].reshape(8,8)
+plt.imshow(some_digit, cmap="gray")
+plt.savefig("./some_digit.png")
+plt.clf()
