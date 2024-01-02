09d0f0022226edce1dc6c8da1bfe65515c379f41
Jakub Cabala <kuba.cabala6@gmail.com>
2024-01-02 22:31:37 +0100

    Cancer model: SVM model created and accuracy evaluated

--- other-projects/thyroid-cancer/model.py
+++ other-projects/thyroid-cancer/model.py
@@ -3,9 +3,11 @@ import numpy as np
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.compose import ColumnTransformer
 from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 # --- Reading data ---
 df = pd.read_csv("./Thyroid_Diff.csv")
@@ -22,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Sorting the columns into categorical and numerical
 numericalColumns, categoricalColumns = [], []
 
-for col in X_train:
+for col in X:
     entry = df[col][0]
     if type(entry) == str:
         categoricalColumns.append(col)
@@ -32,7 +34,8 @@ for col in X_train:
 
 # Transforming columns 
 ct = ColumnTransformer([
-    ('ohe', OneHotEncoder(dtype=int, sparse_output=False, drop='first'), 
+    ('ohe', OneHotEncoder(dtype=int, handle_unknown="ignore", 
+                          sparse_output=False, drop='first'), 
     categoricalColumns),
     ('scalar', StandardScaler(), numericalColumns)
     ])
@@ -41,12 +44,30 @@ ct = ColumnTransformer([
 # --- Dimensional Reduction ---
 pca = PCA(n_components=0.9)
 
+# --- Model 1: SVM ---
+svm = SVC(random_state=0)
+
 # Creating the pipeline
-pipeline = Pipeline([
+svm_pipeline = Pipeline([
     ('ct', ct),
-    ('pca', pca)
+    ('pca', pca),
+    ('svm', svm)
     ])
 
-# Printing the results
-np.set_printoptions(threshold=np.inf)
-print(pipeline.fit_transform(X_train))
+# Grid search
+params = {'svm__C': [1, 5, 8, 10],
+          'svm__kernel': ['linear', 'poly', 'rbf', 'sigmoid']}
+grid_search = GridSearchCV(svm_pipeline, params)
+grid_search.fit(X_train, y_train)
+
+svm_estimator = grid_search.best_estimator_
+
+# Evaluating accuracy on the test set
+y_pred_train = svm_estimator.predict(X_train)
+accuracy_train = accuracy_score(y_train, y_pred_train) 
+
+y_pred_test = svm_estimator.predict(X_test)
+accuracy_test = accuracy_score(y_test, y_pred_test)
+
+print(f"Accuracy of the SVM model on the train set is: {accuracy_train}" 
+      + f"and on a test set is: {accuracy_test}")
